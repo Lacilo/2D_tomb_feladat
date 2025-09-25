825048c4b50d6d0aa8cac8593201b6fba06f7558
Lacilo <szaszfail@kkszki.hu>
2025-09-25 09:34:48 +0200

    kész a program

--- 2D_tomb_feladat/Program.cs
+++ 2D_tomb_feladat/Program.cs
@@ -13,7 +13,45 @@ namespace KetDTombFeladat
     {
         static void Main(string[] args)
         {
-               
+            // NÉHÁNY VÁLTOZÓ INCIALIZÁLÁSA
+            int n = 0;
+            int m = 0;
+            Random rnd = new Random();
+
+
+            // BEKÉRÉS
+            Console.Write("Adja meg a sorok számát \\-$ ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Adja meg az oszlopok számát \\-$ ");
+            m = int.Parse(Console.ReadLine());
+
+
+            // LISTA LÉTREHOZÁSA A MEGADOTT SZÁMOKBÓL
+            int[,] fTomb = new int[n, m];
+
+
+            // FELTÖLTÉS RANDOM SZÁMOKKAL
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    fTomb[i, j] = rnd.Next(1, 10);
+                }
+            }
+
+
+            // KIÍRÁS
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(fTomb[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
